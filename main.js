471de1fb1a7662a3dc0cf1b72b1f50fe69607c40
TetrisUnblocked <104165736+Chipotleguest@users.noreply.github.com>
2023-04-26 18:36:47 -0500

    d

---
+++ main.js
@@ -0,0 +1,15 @@
+// Smooth scrolling effect
+$(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+		e.preventDefault();
+
+		var target = this.hash;
+		var $target = $(target);
+
+		$('html, body').animate({
+			'scrollTop': $target.offset().top
+		}, 1000, 'swing', function () {
+			window.location.hash = target;
+		});
+	});
+});
